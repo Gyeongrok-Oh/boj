6879fd63921f5b3a36ebf9c814f56a3a90084806
owenyi <owenyi3207@gmail.com>
2022-05-08 10:32:06 +0900

    fix 1d-list

--- stage/04_1D-list/3052.py
+++ stage/04_1D-list/3052.py
@@ -1,9 +1,9 @@
 a_list = []
 
-i = 0
-while i < 10:
+_ = 0
+while _ < 10:
     n = int(input())
     a_list.append(n % 42)
-    i += 1
-a_list = set(a_list)
-print(len(a_list))
+    _ += 1
+a_set = set(a_list)
+print(len(a_set))
