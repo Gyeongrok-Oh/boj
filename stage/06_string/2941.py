515010f9830877e68a610a3dbb0a53e76014521d
owenyi <owenyi3207@gmail.com>
2022-06-05 11:39:34 +0900

    add 2941

--- stage/06_string/2941.py
+++ stage/06_string/2941.py
@@ -14,7 +14,10 @@ while i < len(string) - 1:
     substring = string[i:i+2]
     count += 1
     if is_dz(substring):
-        i += 3
+        if i + 2 < len(string) and string[i+2] == '=':
+            i += 3
+        else:
+            i += 1
     elif is_croatia(substring):
         i += 2
     else:
