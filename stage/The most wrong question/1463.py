8c9840f8953557a76f846ee52b6302fe9c2eb5a6
owenyi <owenyi3207@gmail.com>
2022-08-21 11:04:23 +0900

    fix codes

--- stage/The most wrong question/1463.py
+++ stage/The most wrong question/1463.py
@@ -3,15 +3,16 @@ N = int(input())
 cycle = 0
 
 while N != 1:
+    print(N)
     
     if N % 3 == 0:
-        N = N / 3
+        N /= 3
         
     elif N % 2 == 0:
-        N = N / 2
+        N /= 2
         
     else:
-        N = N - 1
+        N -= 1
     
     cycle += 1
 
