5fe534a7046a5fec311aaea5c414cb0addff016e
gyeongrok <0kr3670@gmail.com>
2022-08-30 14:13:31 +0900

    gyeongrok oh

--- stage/11_sort/2750_insertion.py
+++ stage/11_sort/2750_insertion.py
@@ -10,18 +10,75 @@ while _ < N:
 
 cycle = 1
 while cycle < N:
-    key = numbers[cycle]
+
+    key = number[cycle]
     index = cycle - 1
-    while index > -1:
-        
+    while index > - 1:
+
         if numbers[index] > key:
             numbers[index + 1] = numbers[index]
         else:
             break
-        index -= 1
+        index += -1
     numbers[index + 1] = key
     cycle += 1
 
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
 
-for number in numbers:
-    print(number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# N = int(input())
+
+# numbers = []
+
+# _ = 0
+# while _ < N:
+#     number = int(input())
+#     numbers.append(number)
+#     _ += 1
+
+# cycle = 1
+# while cycle < N:
+#     key = numbers[cycle]
+#     index = cycle - 1
+#     while index > -1:
+        
+#         if numbers[index] > key:
+#             numbers[index + 1] = numbers[index]
+#         else:
+#             break
+#         index -= 1
+#     numbers[index + 1] = key
+#     cycle += 1
+
+
+# for number in numbers:
+#     print(number)
