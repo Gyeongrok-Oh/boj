c48ce8db3b83287df029de430e4fc6a239c0a363
gyeongrok <0kr3670@gmail.com>
2022-09-18 09:09:07 +0900

    gyeongrok oh

--- stage/The most wrong question/2609.py
+++ stage/The most wrong question/2609.py
@@ -1,13 +1,34 @@
-x, y = map(int, input(). split())
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+        
+    return a
 
-if x < y :
-    x, y = y , x
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(gcd(a, b))
 
-i = 1
-while i < y + 1:
-    if x % i == 0 and y % i == 0:
-        gcd = i
-    i += 1
 
-print(gcd)
-print(gcd * (x // gcd) * (y // gcd))
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+if __name__ == "__main__":
+    
+    print(lcm(a, b))
+
+
+
+
+# x, y = map(int, input(). split())
+
+# if x < y :
+#     x, y = y , x
+
+# i = 1
+# while i < y + 1:
+#     if x % i == 0 and y % i == 0:
+#         gcd = i
+#     i += 1
+
+# print(gcd)
+# print(gcd * (x // gcd) * (y // gcd))
