7b536acae108ce2977d3fc3feb9039fe52fa9e41
owenyi <owenyi3207@gmail.com>
2022-10-02 11:41:44 +0900

    add 1463 recursion

---
+++ stage/16_DP/1463.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def generate_one(n):
+    if n == 1:
+        return 0
+    root = [generate_one(n - 1)]
+    if n % 3 == 0:
+        root.append(generate_one(n // 3))
+    if n % 2 == 0:
+        root.append(generate_one(n // 2))
+    return min(root) + 1
+
+memo = [-1] * (10**6 + 1)
+
+N = int(input())
+print(generate_one(N))
