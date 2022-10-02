011ffccc5785a001c605424828b7a21439780af7
owenyi <owenyi3207@gmail.com>
2022-10-02 12:03:47 +0900

    add 1463 dp

--- stage/16_DP/1463.py
+++ stage/16_DP/1463.py
@@ -5,7 +5,12 @@ N = int(input())
 
 i = 2
 while i < N + 1:
-    
+    min_list = [memo[i - 1]]
+    if n % 2 == 0:
+        min_list[memo[i // 2]]
+    if n % 3 == 0:
+        min_list[memo[i // 3]]
+    memo[i] = min(min_list) + 1
     i += 1
 
 print(memo[N])
