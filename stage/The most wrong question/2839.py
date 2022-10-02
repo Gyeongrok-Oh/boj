de6aac27fe1dadc5aeff41d91f407e70d03ec069
gyeongrok <0kr3670@gmail.com>
2022-10-02 10:06:13 +0900

    gyeongrok oh

--- stage/The most wrong question/2839.py
+++ stage/The most wrong question/2839.py
@@ -1,14 +1,10 @@
 N = int(input())
 
-if N < 5 and N != 3:
-    print(-1)
-    exit()
-
 result = 0
 
 while N % 5 != 0:
     if N < 0:
-        result += -1
+        result = -1
         print(result)
         exit()
     N -= 3
