c9b0f78322072963d4d8c2e6c1073948f66ae7ca
owenyi <owenyi3207@gmail.com>
2022-10-10 20:32:06 +0900

    fix dfs&bfs

--- stage/The most wrong question/1260.py
+++ stage/The most wrong question/1260.py
@@ -47,6 +47,8 @@ if __name__ == "__main__":
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
+    for i in range(1, V + 1):
+        graph[i].sort()
 
     dfs()
     bfs()
